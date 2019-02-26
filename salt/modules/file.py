433259ff6ab330e0762a47bb3e82c320067c6995
Moritz Bunkus <moritz@bunkus.org>
2019-02-26 19:02:13 +0100

    File module: make file.remove with other types (FIFOs, sockets)
    The current checks only take regular files, symlinks and directories
    into account. For all other types (FIFOs, sockets) the module doesn't
    even attempt to remove them.
    
    The new code only special-cases directories. For all other types of
    existing file system entries a simple os.remove() is attempted.
    
    This is the first part of fixing #35621.

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -3845,6 +3845,10 @@ def remove(path, **kwargs):
     .. code-block:: bash
 
         salt '*' file.remove /tmp/foo
+
+    .. versionchanged:: Neon
+        The method now works on all types of file system entries, not just
+        files, directories and symlinks.
     '''
     path = os.path.expanduser(path)
 
@@ -3852,7 +3856,7 @@ def remove(path, **kwargs):
         raise SaltInvocationError('File path must be absolute: {0}'.format(path))
 
     try:
-        if os.path.isfile(path) or os.path.islink(path):
+        if os.path.islink(path) or (os.path.exists(path) and not os.path.isdir(path)):
             os.remove(path)
             return True
         elif os.path.isdir(path):
