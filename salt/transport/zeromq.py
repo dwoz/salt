a06604b2caba38ed725316e14581600235a18e35
Daniel A. Wozniak <dwozniak@saltstack.com>
2019-05-25 04:36:29 +0000

    zmq transport fix light

--- salt/transport/zeromq.py
+++ salt/transport/zeromq.py
@@ -15,6 +15,7 @@ import logging
 import weakref
 import threading
 from random import randint
+from datetime import timedelta
 
 # Import Salt Libs
 import salt.auth
@@ -64,6 +65,15 @@ except ImportError:
 
 log = logging.getLogger(__name__)
 
+@tornado.gen.coroutine
+def run_future(future):
+    while not future.done():
+        try:
+            yield tornado.gen.with_timeout(timedelta(seconds=1), future)
+        except tornado.gen.TimeoutError:
+            pass
+        yield tornado.gen.moment
+    raise tornado.gen.Return(future.result())
 
 def _get_master_uri(master_ip,
                     master_port,
@@ -283,14 +293,16 @@ class AsyncZeroMQReqChannel(salt.transport.client.ReqChannel):
         :param int tries: The number of times to make before failure
         :param int timeout: The number of seconds on a response before failing
         '''
+
         @tornado.gen.coroutine
         def _do_transfer():
             # Yield control to the caller. When send() completes, resume by populating data with the Future.result
-            data = yield self.message_client.send(
+            fdata =  self.message_client.send(
                 self._package_load(self.auth.crypticle.dumps(load)),
                 timeout=timeout,
                 tries=tries,
             )
+            data = yield run_future(fdata)
             # we may not have always data
             # as for example for saltcall ret submission, this is a blind
             # communication, we do not subscribe to return events, we just
@@ -302,14 +314,14 @@ class AsyncZeroMQReqChannel(salt.transport.client.ReqChannel):
             raise tornado.gen.Return(data)
         if not self.auth.authenticated:
             # Return control back to the caller, resume when authentication succeeds
-            yield self.auth.authenticate()
+            yield run_future(self.auth.authenticate())
         try:
             # We did not get data back the first time. Retry.
-            ret = yield _do_transfer()
+            ret = yield run_future(_do_transfer())
         except salt.crypt.AuthenticationError:
             # If auth error, return control back to the caller, continue when authentication succeeds
-            yield self.auth.authenticate()
-            ret = yield _do_transfer()
+            yield run_future(self.auth.authenticate())
+            ret = yield run_future(_do_transfer())
         raise tornado.gen.Return(ret)
 
     @tornado.gen.coroutine
@@ -321,11 +333,11 @@ class AsyncZeroMQReqChannel(salt.transport.client.ReqChannel):
         :param int tries: The number of times to make before failure
         :param int timeout: The number of seconds on a response before failing
         '''
-        ret = yield self.message_client.send(
+        ret = yield run_future(self.message_client.send(
             self._package_load(load),
             timeout=timeout,
             tries=tries,
-        )
+        ))
 
         raise tornado.gen.Return(ret)
 
@@ -335,9 +347,9 @@ class AsyncZeroMQReqChannel(salt.transport.client.ReqChannel):
         Send a request, return a future which will complete when we send the message
         '''
         if self.crypt == 'clear':
-            ret = yield self._uncrypted_transfer(load, tries=tries, timeout=timeout)
+            ret = yield run_future(self._uncrypted_transfer(load, tries=tries, timeout=timeout))
         else:
-            ret = yield self._crypted_transfer(load, tries=tries, timeout=timeout, raw=raw)
+            ret = yield run_future(self._crypted_transfer(load, tries=tries, timeout=timeout, raw=raw))
         raise tornado.gen.Return(ret)
 
 
@@ -1115,7 +1127,7 @@ class AsyncReqMessageClient(object):
             self.stream.send(message)
 
             try:
-                ret = yield future
+                ret = yield run_future(future)
             except Exception as err:  # pylint: disable=W0702
                 log.debug('Re-init ZMQ socket: %s', err)
                 self._init_socket()  # re-init the zmq socket (no other way in zmq)
@@ -1184,10 +1196,9 @@ class AsyncReqMessageClient(object):
             send_timeout = self.io_loop.call_later(timeout, self.timeout_message, message)
             self.send_timeout_map[message] = send_timeout
 
-        if len(self.send_queue) == 0:
-            self.io_loop.spawn_callback(self._internal_send_recv)
-
         self.send_queue.append(message)
+        if len(self.send_queue) >= 1:
+            self.io_loop.spawn_callback(self._internal_send_recv)
 
         return future
 
