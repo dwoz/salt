54bc163cacaae073ec5d9752e81e0d83f12ee364
Daniel A. Wozniak <dwozniak@vmware.com>
2022-06-29 13:07:12 -0700

    Debian build depends

--- scripts/c/common.h
+++
@@ -1,9 +0,0 @@
-#define PY_SSIZE_T_CLEAN
-#include <libgen.h>
-#include <limits.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <Python.h>
-
-int update_pyhome();
-int run_script(char *name, char *programname);
