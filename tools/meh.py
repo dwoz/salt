98890c01aae064de695542fda62640121c37f0a5
Daniel A. Wozniak <daniel.wozniak@broadcom.com>
2025-01-27 19:17:13 -0700

    m

---
+++ tools/meh.py
@@ -0,0 +1,42 @@
+"""
+"""
+
+import pathlib
+import zipfile
+
+import requests
+
+fwrule = """
+New-NetFirewallRule `
+  -Name sshd `
+  -DisplayName 'OpenSSH SSH Server' `
+  -Enabled True `
+  -Direction Inbound `
+  -Protocol TCP `
+  -Action Allow `
+  -LocalPort 22 `
+  -Program "{}"
+"""
+
+
+def start_ssh_server():
+    """
+    Pretty print the GH Actions event.
+    """
+    resp = requests.get(
+        "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v9.8.1.0p1-Preview/OpenSSH-Win64.zip",
+        allow_redirects=True,
+    )
+    with open("openssh.zip", "wb") as fp:
+        fp.write(resp.content)
+    with zipfile.ZipFile("openssh.zip") as fp:
+        fp.extractall()
+    install_script = pathlib.Path("OpenSSHWin64/install-sshd.ps1").resolve()
+    subprocess.call([f"{install_script}"])
+    with open("fwrule.ps1", "w") as fp:
+        fp.write(fwrule.format(install_script.parent / "sshd.exe"))
+    subprocess.call([f"fwrule.ps1"])
+
+
+if __name__ == "__main__":
+    start_ssh_server()
