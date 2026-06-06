dwoz/salt

commit 78685e5c4432557c96bc18e2002d9e4d6bb1bc03

Provide token storage using the salt.cache interface
Rather than implement custom drivers for tokens, the existing cache
drivers can be leveraged for token management. This also moves token
expiry responsibility to the token(cache) implementation with a naive
fallback.

Implicitly the default token backend will move to either what the global
'cache' is set to, or, if overridden, the eauth_tokens.cache_driver
opt. This means on upgrade, tokens will be invalidated. The only other
token driver is 'rediscluster', which I've added a deprecation for,
since cache.redis_cache provides the same functionality.

salt/auth/__init__.py
@@ -13,18 +13,18 @@ so that any external authentication system can be used inside of Salt
 # 6. Interface to verify tokens
 
 import getpass
+import hashlib
 import logging
+import os
 import random
 import time
 from collections.abc import Iterable, Mapping
 
+import salt.cache
 import salt.channel.client
-import salt.config
 import salt.exceptions
 import salt.loader
-import salt.payload
 import salt.utils.args
-import salt.utils.dictupdate
 import salt.utils.files
 import salt.utils.minions
 import salt.utils.network
@@ -62,6 +62,10 @@ class LoadAuth:
         self.auth = salt.loader.auth(opts)
         self.tokens = salt.loader.eauth_tokens(opts)
         self._ckminions = ckminions
+        tokens_cluster_id = opts["eauth_tokens.cluster_id"] or opts["cluster_id"]
+        self.cache = salt.cache.factory(
+            opts, driver=opts["eauth_tokens.cache_driver"], cluster_id=tokens_cluster_id
+        )
 
     @cached_property
     def ckminions(self):
@@ -235,54 +239,168 @@ class LoadAuth:
         if groups:
             tdata["groups"] = groups
 
-        return self.tokens["{}.mk_token".format(self.opts["eauth_tokens"])](
-            self.opts, tdata
-        )
+        if self.opts["eauth_tokens.cache_driver"] == "rediscluster":
+            salt.utils.versions.warn_until(
+                3010,
+                "The 'rediscluster' token backend has been deprecated, and will be removed "
+                "in the Calcium release. Please use the 'redis_cache' cache backend instead.",
+            )
+            return self.tokens["{}.mk_token".format(self.opts["eauth_tokens"])](
+                self.opts, tdata
+            )
+        else:
+            hash_type = getattr(hashlib, self.opts.get("hash_type", "md5"))
+            new_token = str(hash_type(os.urandom(512)).hexdigest())
+            tdata["token"] = new_token
+            try:
+                self.cache.store("tokens", new_token, tdata, expires=tdata["expire"])
+            except salt.exceptions.SaltCacheError as err:
+                log.error(
+                    "Cannot mk_token from tokens cache using %s: %s",
+                    self.opts["eauth_tokens.cache_driver"],
+                    err,
+                )
+                return {}
+
+            return tdata
 
     def get_tok(self, tok):
         """
         Return the name associated with the token, or False if the token is
         not valid
         """
-        tdata = {}
-        try:
-            tdata = self.tokens["{}.get_token".format(self.opts["eauth_tokens"])](
-                self.opts, tok
+        if self.opts["eauth_tokens.cache_driver"] == "rediscluster":
+            salt.utils.versions.warn_until(
+                3010,
+                "The 'rediscluster' token backend has been deprecated, and will be removed "
+                "in the Calcium release. Please use the 'redis_cache' cache backend instead.",
             )
-        except salt.exceptions.SaltDeserializationError:
-            log.warning("Failed to load token %r - removing broken/empty file.", tok)
-            rm_tok = True
-        else:
-            if not tdata:
+
+            tdata = {}
+            try:
+                tdata = self.tokens["{}.get_token".format(self.opts["eauth_tokens"])](
+                    self.opts, tok
+                )
+            except salt.exceptions.SaltDeserializationError:
+                log.warning(
+                    "Failed to load token %r - removing broken/empty file.", tok
+                )
+                rm_tok = True
+            else:
+                if not tdata:
+                    return {}
+                rm_tok = False
+
+            if tdata.get("expire", 0) < time.time():
+                # If expire isn't present in the token it's invalid and needs
+                # to be removed. Also, if it's present and has expired - in
+                # other words, the expiration is before right now, it should
+                # be removed.
+                rm_tok = True
+
+            if rm_tok:
+                self.rm_token(tok)
                 return {}
-            rm_tok = False
 
-        if tdata.get("expire", 0) < time.time():
-            # If expire isn't present in the token it's invalid and needs
-            # to be removed. Also, if it's present and has expired - in
-            # other words, the expiration is before right now, it should
-            # be removed.
-            rm_tok = True
+            return tdata
+        else:
+            try:
+                tdata = self.cache.fetch("tokens", tok)
 
-        if rm_tok:
-            self.rm_token(tok)
+                if tdata.get("expire", 0) < time.time():
+                    raise salt.exceptions.TokenExpiredError
+
+                return tdata
+            except (
+                salt.exceptions.SaltDeserializationError,
+                salt.exceptions.TokenExpiredError,
+            ):
+                log.warning(
+                    "Failed to load token %r - removing broken/empty file.", tok
+                )
+                self.rm_token(tok)
+            except salt.exceptions.SaltCacheError as err:
+                log.error(
+                    "Cannot get token %s from tokens cache using %s: %s",
+                    tok,
+                    self.opts["eauth_tokens.cache_driver"],
+                    err,
+                )
             return {}
-
-        return tdata
 
     def list_tokens(self):
         """
         List all tokens in eauth_tokens storage.
         """
-        return self.tokens["{}.list_tokens".format(self.opts["eauth_tokens"])](
-            self.opts
-        )
+        if self.opts["eauth_tokens.cache_driver"] == "rediscluster":
+            salt.utils.versions.warn_until(
+                3010,
+                "The 'rediscluster' token backend has been deprecated, and will be removed "
+                "in the Calcium release. Please use the 'redis_cache' cache backend instead.",
+            )
+
+            return self.tokens["{}.list_tokens".format(self.opts["eauth_tokens"])](
+                self.opts
+            )
+        else:
+            try:
+                return self.cache.list("tokens")
+            except salt.exceptions.SaltCacheError as err:
+                log.error(
+                    "Cannot list tokens from tokens cache using %s: %s",
+                    self.opts["eauth_tokens.cache_driver"],
+                    err,
+                )
+                return []
 
     def rm_token(self, tok):
         """
         Remove the given token from token storage.
         """
-        self.tokens["{}.rm_token".format(self.opts["eauth_tokens"])](self.opts, tok)
+        if self.opts["eauth_tokens.cache_driver"] == "rediscluster":
+            salt.utils.versions.warn_until(
+                3010,
+                "The 'rediscluster' token backend has been deprecated, and will be removed "
+                "in the Calcium release. Please use the 'redis_cache' cache backend instead.",
+            )
+
+            self.tokens["{}.rm_token".format(self.opts["eauth_tokens"])](self.opts, tok)
+        else:
+            try:
+                return self.cache.flush("tokens", tok)
+            except salt.exceptions.SaltCacheError as err:
+                log.error(
+                    "Cannot rm token %s from tokens cache using %s: %s",
+                    tok,
+                    self.opts["eauth_tokens.cache_driver"],
+                    err,
+                )
+            return {}
+
+    def clean_expired_tokens(self):
+        """
+        Clean expired tokens
+        """
+        if self.opts["eauth_tokens.cache_driver"] == "rediscluster":
+            salt.utils.versions.warn_until(
+                3010,
+                "The 'rediscluster' token backend has been deprecated, and will be removed "
+                "in the Calcium release. Please use the 'redis_cache' cache backend instead.",
+            )
+            log.debug(
+                "cleaning expired tokens using token driver: {}".format(
+                    self.opts["eauth_tokens"]
+                )
+            )
+            for token in self.list_tokens():
+                token_data = self.get_tok(token)
+                if (
+                    "expire" not in token_data
+                    or token_data.get("expire", 0) < time.time()
+                ):
+                    self.rm_token(token)
+        else:
+            self.cache.clean_expired("tokens")
 
     def authenticate_token(self, load):
         """
@@ -608,6 +726,15 @@ class Resolver:
         load["cmd"] = "get_token"
         tdata = self._send_token_request(load)
         return tdata
+
+    def rm_token(self, token):
+        """
+        Delete a token from the master
+        """
+        load = {}
+        load["token"] = token
+        load["cmd"] = "rm_token"
+        self._send_token_request(load)
 
 
 class AuthUser:

salt/cache/__init__.py
@@ -12,6 +12,7 @@ from collections import OrderedDict
 import salt.config
 import salt.loader
 import salt.syspaths
+from salt.exceptions import SaltCacheError
 from salt.utils.decorators import cached_property
 
 log = logging.getLogger(__name__)
@@ -284,6 +285,32 @@ class Cache:
         fun = f"{self.driver}.contains"
         return self.modules[fun](bank, key, **self.kwargs)
 
+    def clean_expired(self, bank, *args, **kwargs):
+        """
+        Clean expired keys
+
+        :param bank:
+            The name of the location inside the cache which will hold the key
+            and its associated data.
+
+        :raises SaltCacheError:
+            Raises an exception if cache driver detected an error accessing data
+            in the cache backend (auth, permissions, etc).
+        """
+        # If the cache driver has a clean_expired() func, call it to clean up
+        # expired keys.
+        clean_expired = f"{self.driver}.clean_expired"
+        if clean_expired in self.modules:
+            self.modules[clean_expired](bank, *args, **{**self.kwargs, **kwargs})
+        else:
+            list_ = f"{self.driver}.list"
+            updated = f"{self.driver}.updated"
+            flush = f"{self.driver}.flush"
+            for key in self.modules[list_](bank, **self.kwargs):
+                ts = self.modules[updated](bank, key, **self.kwargs)
+                if ts is not None and ts <= time.time():
+                    self.modules[flush](bank, key, **self.kwargs)
+
 
 class MemCache(Cache):
     """
@@ -343,6 +370,8 @@ class MemCache(Cache):
                 (created_at, data) = record
             elif len(record) == 3:
                 (created_at, expires, data) = record
+            else:
+                raise SaltCacheError("Unexpected record structure")
 
             if (created_at + (expires or self.expire)) >= now:
                 if self.debug:

tests/pytests/unit/test_auth.py
@@ -37,7 +37,13 @@ def load_auth():
         patcher = patch(mod, mock)
         patcher.start()
         patchers.append(patcher)
-    lauth = salt.auth.LoadAuth({})  # Load with empty opts
+    lauth = salt.auth.LoadAuth(
+        {
+            "eauth_tokens.cache_driver": None,
+            "eauth_tokens.cluster_id": None,
+            "cluster_id": None,
+        }
+    )  # Load with empty opts
     try:
         yield lauth
     finally:
@@ -213,14 +219,10 @@ def test_get_tok_with_broken_file_will_remove_bad_token(load_auth):
     fake_get_token = MagicMock(
         side_effect=salt.exceptions.SaltDeserializationError("hi")
     )
-    patch_opts = patch.dict(load_auth.opts, {"eauth_tokens": "testfs"})
-    patch_get_token = patch.dict(
-        load_auth.tokens,
-        {"testfs.get_token": fake_get_token},
-    )
+    patch_get_token = patch.object(load_auth.cache, "fetch", fake_get_token)
     mock_rm_token = MagicMock()
     patch_rm_token = patch.object(load_auth, "rm_token", mock_rm_token)
-    with patch_opts, patch_get_token, patch_rm_token:
+    with patch_get_token, patch_rm_token:
         expected_token = "fnord"
         load_auth.get_tok(expected_token)
         mock_rm_token.assert_called_with(expected_token)
@@ -228,14 +230,10 @@ def test_get_tok_with_broken_file_will_remove_bad_token(load_auth):
 
 def test_get_tok_with_no_expiration_should_remove_bad_token(load_auth):
     fake_get_token = MagicMock(return_value={"no_expire_here": "Nope"})
-    patch_opts = patch.dict(load_auth.opts, {"eauth_tokens": "testfs"})
-    patch_get_token = patch.dict(
-        load_auth.tokens,
-        {"testfs.get_token": fake_get_token},
-    )
+    patch_get_token = patch.object(load_auth.cache, "fetch", fake_get_token)
     mock_rm_token = MagicMock()
     patch_rm_token = patch.object(load_auth, "rm_token", mock_rm_token)
-    with patch_opts, patch_get_token, patch_rm_token:
+    with patch_get_token, patch_rm_token:
         expected_token = "fnord"
         load_auth.get_tok(expected_token)
         mock_rm_token.assert_called_with(expected_token)
@@ -243,14 +241,10 @@ def test_get_tok_with_no_expiration_should_remove_bad_token(load_auth):
 
 def test_get_tok_with_expire_before_current_time_should_remove_token(load_auth):
     fake_get_token = MagicMock(return_value={"expire": time.time() - 1})
-    patch_opts = patch.dict(load_auth.opts, {"eauth_tokens": "testfs"})
-    patch_get_token = patch.dict(
-        load_auth.tokens,
-        {"testfs.get_token": fake_get_token},
-    )
+    patch_get_token = patch.object(load_auth.cache, "fetch", fake_get_token)
     mock_rm_token = MagicMock()
     patch_rm_token = patch.object(load_auth, "rm_token", mock_rm_token)
-    with patch_opts, patch_get_token, patch_rm_token:
+    with patch_get_token, patch_rm_token:
         expected_token = "fnord"
         load_auth.get_tok(expected_token)
         mock_rm_token.assert_called_with(expected_token)
@@ -259,14 +253,10 @@ def test_get_tok_with_expire_before_current_time_should_remove_token(load_auth):
 def test_get_tok_with_valid_expiration_should_return_token(load_auth):
     expected_token = {"expire": time.time() + 1}
     fake_get_token = MagicMock(return_value=expected_token)
-    patch_opts = patch.dict(load_auth.opts, {"eauth_tokens": "testfs"})
-    patch_get_token = patch.dict(
-        load_auth.tokens,
-        {"testfs.get_token": fake_get_token},
-    )
+    patch_get_token = patch.object(load_auth.cache, "fetch", fake_get_token)
     mock_rm_token = MagicMock()
     patch_rm_token = patch.object(load_auth, "rm_token", mock_rm_token)
-    with patch_opts, patch_get_token, patch_rm_token:
+    with patch_get_token, patch_rm_token:
         token_name = "fnord"
         actual_token = load_auth.get_tok(token_name)
         mock_rm_token.assert_not_called()
@@ -893,3 +883,38 @@ async def test_acl_simple_deny(auth_acl_clear_funcs, auth_acl_valid_load):
         assert auth_acl_clear_funcs.ckminions.auth_check.call_args[0][0] == [
             {"beta_minion": ["test.ping"]}
         ]
+
+
+def test_cve_2021_3244(tmp_path):
+    token_dir = tmp_path
+    opts = {
+        "extension_modules": "",
+        "optimization_order": [0, 1, 2],
+        "token_expire": 1,
+        "keep_acl_in_token": False,
+        "eauth_tokens": "localfs",
+        "cachedir": str(token_dir),
+        "token_expire_user_override": True,
+        "external_auth": {"auto": {"foo": []}},
+        "eauth_tokens.cache_driver": None,
+        "eauth_tokens.cluster_id": None,
+        "cluster_id": None,
+    }
+    auth = salt.auth.LoadAuth(opts)
+    load = {
+        "eauth": "auto",
+        "username": "foo",
+        "password": "foo",
+        "token_expire": -1,
+    }
+    fake_get_token = MagicMock(
+        side_effect=salt.exceptions.SaltDeserializationError("hi")
+    )
+    with patch.object(auth.cache, "fetch", fake_get_token):
+        t_data = auth.mk_token(load)
+        assert t_data["expire"] < time.time()
+        token_file = token_dir / "tokens" / f'{t_data["token"]}.p'
+        assert token_file.exists()
+        t_data = auth.get_tok(t_data["token"])
+        assert not t_data
+        assert not token_file.exists()

salt/config/__init__.py
@@ -1039,6 +1039,10 @@ VALID_OPTS = immutabletypes.freeze(
         "minimum_auth_version": int,
         # optional cache driver for pillar cache
         "pillar.cache_driver": (type(None), str),
+        # optional cache driver for eauth_tokens cache
+        "eauth_tokens.cache_driver": (type(None), str),
+        # eauth tokens cluster id override
+        "eauth_tokens.cluster_id": (type(None), str),
     }
 )
 
@@ -1723,6 +1727,8 @@ DEFAULT_MASTER_OPTS = immutabletypes.freeze(
         "request_server_ttl": 0,
         "minimum_auth_version": 3,
         "pillar.cache_driver": None,
+        "eauth_tokens.cache_driver": None,
+        "eauth_tokens.cluster_id": None,
     }
 )
 

salt/pillar/__init__.py
@@ -1306,7 +1306,7 @@ class PillarCache(Pillar):
 
     def clear_pillar(self):
         """
-        Clea the pillar cache, if it exists
+        Clear the pillar cache, if it exists
         """
         return self.cache.flush("pillar", self.pillar_key)
 

tests/pytests/unit/auth/test_auth.py
@@ -1,33 +1,0 @@
-import time
-
-import salt.auth
-import salt.config
-
-
-def test_cve_2021_3244(tmp_path):
-    token_dir = tmp_path / "tokens"
-    token_dir.mkdir()
-    opts = {
-        "extension_modules": "",
-        "optimization_order": [0, 1, 2],
-        "token_expire": 1,
-        "keep_acl_in_token": False,
-        "eauth_tokens": "localfs",
-        "token_dir": str(token_dir),
-        "token_expire_user_override": True,
-        "external_auth": {"auto": {"foo": []}},
-    }
-    auth = salt.auth.LoadAuth(opts)
-    load = {
-        "eauth": "auto",
-        "username": "foo",
-        "password": "foo",
-        "token_expire": -1,
-    }
-    t_data = auth.mk_token(load)
-    assert t_data["expire"] < time.time()
-    token_file = token_dir / t_data["token"]
-    assert token_file.exists()
-    t_data = auth.get_tok(t_data["token"])
-    assert not t_data
-    assert not token_file.exists()

salt/daemons/masterapi.py
@@ -141,10 +141,7 @@ def clean_expired_tokens(opts):
     Clean expired tokens from the master
     """
     loadauth = salt.auth.LoadAuth(opts)
-    for tok in loadauth.list_tokens():
-        token_data = loadauth.get_tok(tok)
-        if "expire" not in token_data or token_data.get("expire", 0) < time.time():
-            loadauth.rm_token(tok)
+    loadauth.clean_expired_tokens()
 
 
 def clean_pub_auth(opts):

salt/exceptions.py
@@ -350,6 +350,12 @@ class TokenAuthenticationError(SaltException):
     """
 
 
+class TokenExpiredError(SaltException):
+    """
+    Thrown when token is expired
+    """
+
+
 class SaltDeserializationError(SaltException):
     """
     Thrown when salt cannot deserialize data.

commit 39712e60da9f50b717de4f3913ef8b2ad9fb4028

Fix premature deletion of localfs auth tokens
Two bugs introduced in #68039 combined to delete every auth token
from the default localfs cache backend within one master
``loop_interval`` (default 60s):

1. ``Cache.clean_expired``'s fallback (for drivers without a native
   ``clean_expired``) compared the driver's ``updated()`` value --
   for localfs the file mtime, an epoch in the past -- with
   ``time.time()`` and flushed every key whose mtime had passed.
   That is true for every file on disk, so the entire bank was
   wiped on every sweep.

2. ``LoadAuth.mk_token`` passed ``expires=tdata["expire"]`` to
   ``Cache.store``, where ``tdata["expire"]`` is the absolute epoch
   ``time.time() + token_expire``. ``Cache.store``'s ``expires``
   parameter is documented as a relative duration in seconds, so the
   envelope ``_expires`` was being written as ``now + (now +
   token_expire)`` -- roughly year 4090.

Replace the fallback with one that consults the ``_expires``
envelope ``Cache.store`` writes (entries with no envelope have no
cache-level expiry and are left alone), and pass the relative
``token_expire`` duration from ``mk_token``. Adds regression
coverage at both the cache-driver and ``LoadAuth`` layers.

Fixes #69307

salt/auth/__init__.py
@@ -253,7 +253,15 @@ class LoadAuth:
             new_token = str(hash_type(os.urandom(512)).hexdigest())
             tdata["token"] = new_token
             try:
-                self.cache.store("tokens", new_token, tdata, expires=tdata["expire"])
+                # ``Cache.store``'s ``expires`` is a *relative* duration in
+                # seconds, not an absolute epoch. Passing ``tdata["expire"]``
+                # here -- which is ``time.time() + token_expire`` -- caused
+                # the envelope ``_expires`` to be set to ``now + (now +
+                # token_expire)`` (~ year 4090), and combined with the
+                # broken ``Cache.clean_expired`` fallback resulted in tokens
+                # being deleted within a single master loop interval.
+                # Issue #69307.
+                self.cache.store("tokens", new_token, tdata, expires=token_expire)
             except salt.exceptions.SaltCacheError as err:
                 log.error(
                     "Cannot mk_token from tokens cache using %s: %s",

salt/cache/__init__.py
@@ -332,14 +332,32 @@ class Cache:
         clean_expired = f"{self.driver}.clean_expired"
         if clean_expired in self.modules:
             self.modules[clean_expired](bank, *args, **{**self.kwargs, **kwargs})
-        else:
-            list_ = f"{self.driver}.list"
-            updated = f"{self.driver}.updated"
-            flush = f"{self.driver}.flush"
-            for key in self.modules[list_](bank, **self.kwargs):
-                ts = self.modules[updated](bank, key, **self.kwargs)
-                if ts is not None and ts <= time.time():
-                    self.modules[flush](bank, key, **self.kwargs)
+            return
+
+        # Fallback for drivers without native clean_expired. Use the
+        # ``_expires`` envelope written by ``Cache.store`` when ``expires``
+        # is supplied; entries without that envelope have no cache-level
+        # expiry and are left alone. (Previously this path treated the
+        # driver's ``updated()`` value -- the file mtime for ``localfs`` --
+        # as an absolute expiry epoch, which deleted every entry whose
+        # mtime was in the past, i.e. every entry. Issue #69307.)
+        list_ = f"{self.driver}.list"
+        fetch = f"{self.driver}.fetch"
+        flush = f"{self.driver}.flush"
+        now = time.time()
+        for key in self.modules[list_](bank, **self.kwargs):
+            try:
+                raw = self.modules[fetch](bank, key, **self.kwargs)
+            except SaltCacheError:
+                # Best-effort: don't let one unreadable key abort the sweep.
+                log.debug("clean_expired: unable to read %s/%s; skipping", bank, key)
+                continue
+            if (
+                isinstance(raw, dict)
+                and set(raw.keys()) == {"data", "_expires"}
+                and raw["_expires"] <= now
+            ):
+                self.modules[flush](bank, key, **self.kwargs)
 
 
 class MemCache(Cache):

tests/pytests/functional/cache/test_localfs.py
@@ -1,5 +1,6 @@
 import logging
 import shutil
+import time
 from pathlib import Path
 
 import pytest
@@ -82,3 +83,49 @@ def test_contains_is_constrained_to_cachedir(cache, tmp_path, key):
     if key is not None:
         (tmp_path / f"{key}.p").touch()
     assert not cache.contains(str(tmp_path), key)
+
+
+def test_clean_expired_does_not_drop_unexpired_entries_69307(cache):
+    """
+    Regression test for issue #69307.
+
+    ``Cache.clean_expired`` falls back to ``driver.updated()`` for backends
+    (like ``localfs``) that don't expose their own ``clean_expired``. The
+    fallback previously compared ``updated()`` (the file mtime, an epoch
+    in the past) against ``time.time()`` and flushed every key whose
+    mtime had passed -- i.e. every key on disk. That deleted freshly
+    minted auth tokens within one master ``loop_interval`` (default 60s),
+    regardless of the token's actual expiry.
+
+    A freshly stored entry with a ``_expires`` envelope that points into
+    the future, and a plain entry with no envelope at all, must both
+    survive ``clean_expired``.
+    """
+    bank = "tokens"
+    key_with_envelope = "tok-with-envelope"
+    key_plain = "tok-plain"
+
+    # Plain store: no expires kwarg -> no envelope.
+    cache.store(bank, key_plain, {"name": "alice", "expire": time.time() + 12 * 3600})
+
+    # Store with a far-future expires duration (12 hours).
+    cache.store(
+        bank,
+        key_with_envelope,
+        {"name": "bob", "expire": time.time() + 12 * 3600},
+        expires=12 * 3600,
+    )
+
+    assert cache.contains(bank, key_plain)
+    assert cache.contains(bank, key_with_envelope)
+
+    cache.clean_expired(bank)
+
+    assert cache.contains(bank, key_plain), (
+        "Plain (non-enveloped) token was flushed by clean_expired even "
+        "though it has no expiry information; the fallback must not "
+        "treat file mtime as an absolute expiry epoch (issue #69307)."
+    )
+    assert cache.contains(bank, key_with_envelope), (
+        "Token with future expiry was flushed by clean_expired " "(issue #69307)."
+    )

tests/pytests/unit/test_auth.py
@@ -936,6 +936,63 @@ async def test_acl_simple_deny(auth_acl_clear_funcs, auth_acl_valid_load):
         ]
 
 
+def test_mk_token_survives_clean_expired_tokens_69307(tmp_path):
+    """
+    Regression test for issue #69307.
+
+    A freshly minted token must not be wiped by the very next iteration
+    of the master ``clean_expired_tokens`` loop. The bug: ``Cache.store``
+    expects ``expires`` to be a relative duration in seconds, but
+    ``LoadAuth.mk_token`` was passing the absolute epoch
+    ``time.time() + token_expire``. Worse, ``Cache.clean_expired``'s
+    fallback used the file mtime as if it were an absolute expiry
+    epoch, so every token on disk was flushed unconditionally.
+
+    With the fix:
+    - ``LoadAuth.mk_token`` passes a relative duration to ``Cache.store``.
+    - ``Cache.clean_expired``'s fallback respects the stored token's
+      ``expire`` (or the ``_expires`` envelope) rather than file mtime.
+    The token must still be retrievable after
+    ``clean_expired_tokens()``.
+    """
+    opts = {
+        "extension_modules": "",
+        "optimization_order": [0, 1, 2],
+        # Long-lived token (12 hours) — the default.
+        "token_expire": 12 * 60 * 60,
+        "keep_acl_in_token": False,
+        "eauth_tokens": "localfs",
+        "cachedir": str(tmp_path),
+        "token_expire_user_override": False,
+        "external_auth": {"auto": {"foo": []}},
+        "eauth_tokens.cache_driver": None,
+        "eauth_tokens.cluster_id": None,
+        "cluster_id": None,
+        "hash_type": "sha256",
+    }
+    auth = salt.auth.LoadAuth(opts)
+    load = {"eauth": "auto", "username": "foo", "password": "foo"}
+
+    tdata = auth.mk_token(load)
+    assert tdata, "mk_token should return the token data on success"
+    token = tdata["token"]
+    # Sanity: the token should be retrievable straight away.
+    assert auth.get_tok(token).get("token") == token
+
+    # Simulate the master loop that runs every ``loop_interval`` seconds
+    # and calls ``LoadAuth.clean_expired_tokens()`` on the auth backend.
+    # With the bug, this call deletes the token from disk even though
+    # its real ``expire`` is 12 hours away.
+    auth.clean_expired_tokens()
+
+    surviving = auth.get_tok(token)
+    assert surviving, (
+        "Token was deleted by clean_expired_tokens even though it was "
+        "just minted with a 12-hour expiry. Issue #69307."
+    )
+    assert surviving.get("token") == token
+
+
 def test_cve_2021_3244(tmp_path):
     token_dir = tmp_path
     opts = {